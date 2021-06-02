4432ec832f5f5f773d34aabd5a716ca9aa37d3ca
Daniel <daniel.rosendos@hotmail.com>
2021-06-02 20:25:56 -0300

    finalizando aula 6

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    CommonModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
   declarations: [
     AppComponent
-  ]
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+}
