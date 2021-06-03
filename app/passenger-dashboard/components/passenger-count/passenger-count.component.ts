eadbbd169b9f8f06e2c19d932e7eaade384a6dcb
Daniel <daniel.rosendos@hotmail.com>
2021-06-02 23:06:09 -0300

    finalizando aula 25

--- app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -16,9 +16,6 @@ export class PassengerCountComponent {
   @Input() items: Passenger[];
   checkedInCount(): number {
     if (!this.items) return;
-
-    return this.items.filter((passenger: Passenger) => {
-      return passenger.checkedIn;
-    }).length;
+    return this.items.filter((passenger: Passenger) => passenger.checkedIn).length;
   }
 }
