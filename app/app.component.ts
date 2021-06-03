875aae4f56f54a7f1351e53d7814b096b3169a67
Daniel <daniel.rosendos@hotmail.com>
2021-06-02 21:12:53 -0300

    finalizando aula 10

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,19 +5,28 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <h1 [innerHTML]="title" ></h1>
-      <h1>{{ title }}</h1>
-      <img [src]="logo">
-      <input type="text" [value]="name" >
+      <button (click)="handleClick()">
+        Change Name
+      </button>
+      <input
+        type="text"
+        [value]="name"
+        (input)="handleIput($event)"
+        (blur)="handleBlur($event)"
+      >
       <div>{{ name }}</div>
     </div>
   `
 })
 export class AppComponent {
-  title: string;
   name: string = 'Todd';
-  logo: string = 'img/logo.svg';
-  constructor() {
-    this.title = 'Ultimate Angular';
+  handleBlur(event: any) {
+    this.name = event.target.value;
+  }
+  handleIput(event: any) {
+    this.name = event.target.value;
+  }
+  handleClick() {
+    this.name = 'Motto';
   }
 }
