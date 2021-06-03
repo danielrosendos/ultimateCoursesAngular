bcdbaabd5c60ed0e0a77149a3104e2db2ccf1d4d
Daniel <daniel.rosendos@hotmail.com>
2021-06-02 21:56:31 -0300

    finalizando aula 1

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,18 +16,30 @@ interface Passenger {
         <li *ngFor="let passenger of passengers; let i = index">
           <span
             class="status"
-            [class.checked-in]="passenger.checkedIn"></span>
-          {{ i }}: {{ passenger.fullname }}
-        </li>
-      </ul>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
             [ngClass]="{
                 'checked-in': passenger.checkedIn,
                 'checked-out': !passenger.checkedIn
             }"></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index">
+          <span
+            class="status"
+            [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')"
+          ></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index">
+          <span
+            class="status"
+            [ngStyle]="{ backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')}"
+          ></span>
           {{ i }}: {{ passenger.fullname }}
         </li>
       </ul>
