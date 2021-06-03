248cb43a11eb36c3a9b5292f3147ea5f7f0af415
Daniel <daniel.rosendos@hotmail.com>
2021-06-03 14:47:57 -0300

    finalizando aula 46

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { PassengerDashboardModule } from "./passenger-dashboard/passenger.module";
@@ -14,6 +15,7 @@ import { AppComponent } from "./app.component";
   imports: [
     FormsModule,
     CommonModule,
+    RouterModule,
     BrowserModule,
     PassengerDashboardModule
   ],
