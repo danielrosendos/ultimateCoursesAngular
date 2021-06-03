60d8c4420e6facc64b78de7807e961136b0afbe3
Daniel <daniel.rosendos@hotmail.com>
2021-06-03 14:36:25 -0300

    finalizando aula 43

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -14,8 +14,13 @@ import {Baggage} from "../../models/baggage.interface";
         <input
           type="text"
           name="fullname"
+          required
+          #fullname="ngModel"
           [ngModel]="detail?.fullname"
         >
+        <div *ngIf="fullname.errors?.required && fullname.dirty" class="error">
+          Passenger name is required
+        </div>
       </div>
 
       <div>
@@ -23,8 +28,13 @@ import {Baggage} from "../../models/baggage.interface";
         <input
           type="number"
           name="id"
+          required
+          #id="ngModel"
           [ngModel]="detail?.id"
         >
+        <div *ngIf="id.errors?.required && id.dirty" class="error">
+          Passenger id is required
+        </div>
       </div>
 
       <div>
@@ -74,7 +84,15 @@ import {Baggage} from "../../models/baggage.interface";
         </select>
       </div>
 
-      {{form.value | json}}
+      <div>
+        {{form.value | json}}
+      </div>
+      <div>
+        Form Valid: {{form.valid | json}}
+      </div>
+      <div>
+        Form Invalid: {{form.invalid | json}}
+      </div>
     </form>
   `
 })
