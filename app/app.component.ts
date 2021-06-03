5f1fb20dc6dc2ad8a8b898de2769c13f794b8bd1
Daniel <daniel.rosendos@hotmail.com>
2021-06-03 15:08:38 -0300

    finalizando aula 50

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <a routerLink="/">
-        Home
-      </a>
-      <a routerLink="/oops">
-        404
-      </a>
+      <nav class="nav">
+        <a
+          routerLink="/"
+          routerLinkActive="active"
+          [routerLinkActiveOptions]="{ exact: true}"
+        >
+          Home
+        </a>
+        <a
+          routerLink="/oops"
+          routerLinkActive="active"
+        >
+          404
+        </a>
+      </nav>
       <router-outlet></router-outlet>
     </div>
   `
