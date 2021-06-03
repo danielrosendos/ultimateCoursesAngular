8949c327ddb3d9ef5dbd8600d172a4f4a5886ae8
Daniel <daniel.rosendos@hotmail.com>
2021-06-03 14:54:09 -0300

    finalizaando aula 47

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { PassengerDashboardModule } from "./passenger-dashboard/passenger.module";
 
+import { HomeComponent } from "./home.component";
 import { AppComponent } from "./app.component";
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     FormsModule,
     CommonModule,
-    RouterModule,
+    RouterModule.forRoot(routes),
     BrowserModule,
     PassengerDashboardModule
   ],
